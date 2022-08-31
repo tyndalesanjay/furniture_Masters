3c8a9c9fabbf8cb0bcc5d2910ba90b9f43e9b331
tyndalesanjay <60053448+tyndalesanjay@users.noreply.github.com>
2022-08-31 12:18:09 -0500

    Created new repo

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const exec = require('child_process').exec
 
 const config = require('./config/db');
 const userRoute = require('./routes/user.routes');
@@ -41,11 +40,6 @@ app.use('/api/items_list', itemRoute);
 app.use('/api/users', userRoute);
 app.use('/api/Cart', cartRoute);
 
-exec("dir")
-app.get("*.*", express.static('../Shoppping-app/dist/shoppping-app/', { maxAge: '1y' }))
-app.get("/*", (req, res) => {
-  res.status(200).sendFile('/', { root: '../Shoppping-app/dist/shoppping-app' })
-})
 
 
 
