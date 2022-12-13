2107e1f9258111db27e4480be137cc48e7c7e958
tyndalesanjay <60053448+tyndalesanjay@users.noreply.github.com>
2022-12-12 20:11:26 -0500

    Updated to function with firebase

--- backend/routes/order.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const order = require('../controller/order.controller')
-const router = express.Router();
-
-router.route("/").get(order.getOrders).post(order.createOrder);
-
-router.route("/:id").get(order.getOrderById).put(order.updateOrder).delete(order.deleteOrder);
-
-module.exports = router;
